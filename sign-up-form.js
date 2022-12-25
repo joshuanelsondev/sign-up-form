71637183ce05f5ce3829046c075e0c4a4261d594
joshuanelsondev <joshuanelsondev@gmail.com>
2022-12-24 22:32:31 -0500

    Edit input stylings

--- sign-up-form.js
+++ sign-up-form.js
@@ -17,84 +17,103 @@ const submitButton = document.querySelector('.submit');
 const confirmation = document.createElement('div');
 confirmation.classList.add('confirm');
 const form = document.querySelector('form');
-let validCounter = 0;
 
 
-
-firstName.addEventListener('input', () => {
-    
+firstName.addEventListener('blur', () => {
+    confirmation.textContent = '';
     if (firstName.value === '') {
-        console.log('test');
         firstNameErrorMessage.textContent = 'Please enter your first name';
         firstNameErrorMessage.classList.add('errorMessage');
         firstName.after(firstNameErrorMessage);
+        firstName.style.cssText = 'border: 2px solid red;';
     } else {
         firstNameErrorMessage.textContent = '';
-        validCounter++;
     }
 }); 
 
-lastName.addEventListener('input', (event) => {
+firstName.addEventListener('focus', () => {
+    firstNameErrorMessage.textContent = '';
+    firstName.style.border = 'none';
+});
+
+lastName.addEventListener('blur', (event) => {
     if (lastName.value === '') {
         lastNameErrorMessage.textContent = 'Please enter your last name';
         lastNameErrorMessage.classList.add('errorMessage');
         lastName.after(lastNameErrorMessage);
+        lastName.style.cssText = 'border: 2px solid red';
     } else {
         lastNameErrorMessage.textContent = '';
-        validCounter++;
-
     }
 });
 
-email.addEventListener('input', (event) => {
+lastName.addEventListener('focus', () => {
+    lastNameErrorMessage.textContent = '';
+    lastName.style.border = 'none';
+});
+
+email.addEventListener('blur', (event) => {
     if (email.validity.typeMismatch) {
         emailErrorMessage.textContent = 'Please enter a valid email';
         emailErrorMessage.classList.add('errorMessage');
         email.after(emailErrorMessage);
+        email.style.cssText = 'border: 2px solid red';
     } else {
         emailErrorMessage.textContent = '';
-        validCounter++;
-
     }
 });
 
-phone.addEventListener('input', (event) => {
-    if (phone.validity.patternMismatch) {
+email.addEventListener('focus', (event) => {
+    emailErrorMessage.textContent = '';
+    email.style.border = 'none';
+});
+
+phone.addEventListener('blur', (event) => {
+    if (phone.value.length <= 10 && phone.validity.patternMismatch) {
         phoneErrorMessage.textContent = 'Please enter a valid phone number';
         phoneErrorMessage.classList.add('errorMessage');
         phone.after(phoneErrorMessage);
     } else {
         phoneErrorMessage.textContent = '';
-        validCounter++;
-
     }
+}); 
+
+phone.addEventListener('focus', (event) => {
+    phoneErrorMessage.textContent = '';
+    phone.style.border = 'none';
 });
 
-password.addEventListener('input', (event) => {
+password.addEventListener('blur', (event) => {
     if (password.validity.patternMismatch) {
         passwordErrorMessage.textContent = 'Must inlcude 1 number, 1 capital letter, at least 8 characters';
         passwordErrorMessage.classList.add('pwdErrorMessage');
         showPwdDiv.after(passwordErrorMessage);
     } else {
         passwordErrorMessage.textContent = '';
-        validCounter++;
-
     }
 });
 
-confirmPassword.addEventListener('input', (event) => {
+password.addEventListener('focus', (event) => {
+    passwordErrorMessage.textContent = '';
+    password.style.border = 'none';
+});
+
+confirmPassword.addEventListener('blur', (event) => {
     if (password.value !== confirmPassword.value) {
         confirmPasswordErrorMessage.textContent = 'Passwords do not match';
         confirmPasswordErrorMessage.classList.add('pwdErrorMessage');
         confirmPassword.after(confirmPasswordErrorMessage);
     } else {
         confirmPasswordErrorMessage.textContent = '';
-        validCounter++;
-
     }
 });
 
-submitButton.addEventListener('click', confirmAccountCreation);
+confirmPassword.addEventListener('focus', (event) => {
+    confirmPasswordErrorMessage.textContent = '';
+    confirmPassword.style.border = 'none';
+});
+
+form.addEventListener('submit', confirmAccountCreation);
 
 
 function showPassword() {
@@ -107,14 +126,8 @@ function showPassword() {
 
 function confirmAccountCreation(event) {
     event.preventDefault();
-    if (validCounter > 5 && confirmation.textContent == '') {
-        confirmation.textContent = "Thank you for signing up with the Odin Café!";
-        form.append(confirmation);
-        setInterval(() => {
-            confirmation.textContent = '';
-        }, '10000');
-        validCounter = 0;
-    }
+    console.log('Test');
+    confirmation.textContent = "Thank you for signing up with the Odin Café!";
+    form.append(confirmation); 
     form.reset();
-    
 }
